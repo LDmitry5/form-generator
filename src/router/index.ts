363966e8457112ac5d28b1743796f63bd85a9aaa
Dmitry Likhachev <kolt_1991@mail.ru>
2025-12-27 17:40:07 +0300

    fix: process.env.NODE_ENV

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,13 @@ import DemoForm1 from "@/pages/DemoForm1.vue";
 import DemoForm2 from "@/pages/DemoForm2.vue";
 
 const routes = [
-  { path: "/", component: Home, alias: "" },
+  { path: "/", component: Home },
   { path: "/form1", component: DemoForm1 },
   { path: "/form2", component: DemoForm2 },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.NODE_ENV === "production" ? "/form-generator/" : "/"),
   routes,
 });
 
