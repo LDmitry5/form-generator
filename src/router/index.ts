8528b3a9859e40659e719378edd3176f7fca3631
Dmitry Likhachev <kolt_1991@mail.ru>
2025-12-27 17:28:01 +0300

    fix: alias at home

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import DemoForm1 from "@/pages/DemoForm1.vue";
 import DemoForm2 from "@/pages/DemoForm2.vue";
 
 const routes = [
-  { path: "/", component: Home },
+  { path: "/", component: Home, alias: "" },
   { path: "/form1", component: DemoForm1 },
   { path: "/form2", component: DemoForm2 },
 ];
